477017e95d8d96e18828b8c5167624d5aaa91431
surinoel <yyj48921@gmail.com>
2019-03-27 01:51:44 +0900

    lsp01

--- lsp01.c
+++ lsp01.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+int write_data(void) {
+    FILE *fp;
+    fp = fopen("data", "w");
+    
+    if(fp == NULL) {
+        perror("File not open\n");
+        return -1;
+    }
+
+    fputs("hello world!\n", fp);
+    return 0;
+}
+
 int main(int argc, char **argv) {
-    printf("hello world!\n");
+    write_data();
     return 0;
 }
