94397daacf8c8baf317baa57f3063d2cafd1b5c8
surinoel <48958763+surinoel@users.noreply.github.com>
2019-04-16 18:34:54 +0900

    Update and rename lsp02.c to append.c

---
+++ append.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int append_data(void) {
+    FILE *fp;
+    
+    if(!(fp = fopen("datafile", "a"))) {
+        printf("fopen error\n");
+        return -1;
+    }
+
+    fputs("it's ubuntu environment\n", fp);
+    
+    fclose(fp);
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    if(append_data()) {
+
+    }
+    return 0;
+}
