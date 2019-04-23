8b3762c1b20a4a4d3479d37431400e4c77aa8a2b
surinoel <48958763+surinoel@users.noreply.github.com>
2019-04-24 03:36:18 +0900

    Update lsp08.c

--- lsp08.c
+++ lsp08.c
@@ -1,33 +1,35 @@
+/*
+int open(const char *pathname, int flags, mode_t mode);
+flags must include one of the following access modes: O_RDONLY, O_WRONLY, or O_RDWR.  These request opening  the  file  read-only, write-only, or read/write, respectively.
+
+system call
+man 2 open
+*/
+
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <unistd.h>
 
-static int write_file(void) {
+int main(int argc, char **argv) {
     int fd;
-    char name[20];
+    char name[50];
+    
     memset(name, 0, sizeof(name));
-    memcpy(name, "유영재", sizeof(name));
+    memcpy(name, "리눅스 시스템 프로그래밍", sizeof(name));
 
     fd = open("datafile", O_WRONLY | O_CREAT, 0664);
     if(fd < 0) {
         printf("open() error\n");
         return -1;
     }
-    
+
     dprintf(fd, "Hello world\n");
-    dprintf(fd, "My name is %s\n", name);
+    dprintf(fd, "It's %s\n", name);
 
     close(fd);
-    return 0;
-}
 
-int main(int argc, char **argv) {
-    if(write_file()) {
-        perror("write error\n");
-        return -1;
-    }
     return 0;
 }
