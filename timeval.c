75569d85686440bc566a289179a6aa15aaaa76b9
surinoel <48958763+surinoel@users.noreply.github.com>
2019-05-22 17:57:05 +0900

    Update timeval.c

--- timeval.c
+++ timeval.c
@@ -9,13 +9,12 @@ int main(int argc, char **argv) {
     struct timeval tv;
    
     #if 0
-        gettimeofday에서 두번째 인자인 struct timezone 변수는 내부적으로 사용되지 않는다
+    gettimeofday에서 두번째 인자인 struct timezone 변수는 내부적으로 사용되지 않는다
     
     struct timeval {
         time_t      tv_sec;     /* seconds */
         suseconds_t tv_usec;    /* microseconds */
     };
-
     #endif
         
     if(gettimeofday(&tv, NULL)) { 
