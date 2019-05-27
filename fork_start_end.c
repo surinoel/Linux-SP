14398ebc5741e56bb8c599d383a87a423ef8a465
surinoel <48958763+surinoel@users.noreply.github.com>
2019-05-27 23:41:20 +0900

    Create fork_start_end.c

---
+++ fork_start_end.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char **argv) {
+    pid_t pid;
+    int exit_status;
+
+    pid = fork();
+    if(pid == 0) {
+        printf("It is child process\n");
+        exit(EXIT_SUCCESS);
+    }
+    else if(pid > 0) {
+        printf("It is parent process\n");
+        wait(&exit_status);
+        if(WIFEXITED(exit_status)) {
+            printf("child process exited normally\n");
+        }
+    }
+    
+    printf("fork end...\n");
+    return 0;
+}
+
+
+#if 0
+It is parent process
+It is child process
+child process exited normally
+fork end...
+#endif
